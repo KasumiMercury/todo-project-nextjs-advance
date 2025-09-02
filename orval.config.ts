481c1613fe68932380d568ac114d76bfd859e545
KasumiMercury <naoki_shira@outlook.jp>
2025-09-02 23:34:47 +0900

    chore: add Orval configuration for task and user schemas

---
+++ orval.config.ts
@@ -0,0 +1,53 @@
+import {defineConfig} from 'orval';
+
+
+export default defineConfig({
+  task: {
+    input: './schema/task-server.yaml',
+    output: {
+      target: "./src/client/task",
+      schemas: "./src/client/task/schemas",
+      client: "fetch",
+      httpClient: "fetch",
+      override: {
+        mutator: {
+          path: "./src/client/fetch/taskFetch.ts",
+          name: "taskFetch",
+        },
+      },
+      mode: "tags-split",
+      mock: {
+        type: "msw",
+        useExamples: true,
+        generateEachHttpStatus: true,
+        indexMockFiles: true
+      },
+      biome: true,
+      clean: true,
+    },
+  },
+  user: {
+    input: './schema/user-server.json',
+    output: {
+      target: "./src/client/user",
+      schemas: "./src/client/user/schemas",
+      client: "fetch",
+      httpClient: "fetch",
+      override: {
+        mutator: {
+          path: "./src/client/fetch/userFetch.ts",
+          name: "userFetch",
+        },
+      },
+      mode: "tags-split",
+      mock: {
+        type: "msw",
+        useExamples: true,
+        generateEachHttpStatus: true,
+        indexMockFiles: true
+      },
+      biome: true,
+      clean: true,
+    },
+  },
+});
