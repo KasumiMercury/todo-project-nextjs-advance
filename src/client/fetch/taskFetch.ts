06e348d3dbfd426bd4e2f220444bf6ded0cb3257
KasumiMercury <naoki_shira@outlook.jp>
2025-09-03 00:25:44 +0900

    style: format

--- src/client/fetch/taskFetch.ts
+++ src/client/fetch/taskFetch.ts
@@ -1,10 +1,11 @@
-import {customFetch} from "@/client/fetch/fetch-utils";
+import { customFetch } from "@/client/fetch/fetch-utils";
 
-const baseUrl = process.env.NEXT_PUBLIC_TASK_API_BASE_URL || "http://localhost:8080";
+const baseUrl =
+	process.env.NEXT_PUBLIC_TASK_API_BASE_URL || "http://localhost:8080";
 
 export const taskFetch = async <T>(
-    url: string,
-    options: RequestInit,
+	url: string,
+	options: RequestInit,
 ): Promise<T> => {
-  return customFetch(baseUrl, url, options);
+	return customFetch(baseUrl, url, options);
 };
--- src/client/fetch/userFetch.ts
+++ src/client/fetch/userFetch.ts
@@ -1,10 +1,11 @@
-import {customFetch} from "@/client/fetch/fetch-utils";
+import { customFetch } from "@/client/fetch/fetch-utils";
 
-const baseUrl = process.env.NEXT_PUBLIC_USER_API_BASE_URL || "http://localhost:8080";
+const baseUrl =
+	process.env.NEXT_PUBLIC_USER_API_BASE_URL || "http://localhost:8080";
 
 export const userFetch = async <T>(
-    url: string,
-    options: RequestInit,
+	url: string,
+	options: RequestInit,
 ): Promise<T> => {
-  return customFetch(baseUrl, url, options);
+	return customFetch(baseUrl, url, options);
 };
