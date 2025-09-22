acb152c73c6cfd53c1a383b390dbc408c9765299
KasumiMercury <naoki_shira@outlook.jp>
2025-09-22 16:06:49 +0900

    feat: add reusable Button, Card, Input, and Label components

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export const Label = React.forwardRef<
+  HTMLLabelElement,
+  React.LabelHTMLAttributes<HTMLLabelElement>
+>(({ className, children, ...props }, ref) => (
+  <>
+    {/* biome-ignore lint/a11y/noLabelWithoutControl: association handled by consumers via props */}
+    <label
+      ref={ref}
+      className={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </label>
+  </>
+));
+
+Label.displayName = "Label";
